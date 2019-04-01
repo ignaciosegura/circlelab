60885cbe4f26cf2ffb22026a16305529fafc3738
Ignacio Segura <nacho@isegura.es>
2019-04-01 11:31:55 +0200

    Updates

---
+++ precache-manifest.6c0ac83e7fbf65d76042e681d1e2044d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0225ec4a1fe633afdad7",
+    "url": "/circlelab/static/js/runtime~main.0f9e822d.js"
+  },
+  {
+    "revision": "41aec126d2f469836f9a",
+    "url": "/circlelab/static/js/main.cb6a41f7.chunk.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/circlelab/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "41aec126d2f469836f9a",
+    "url": "/circlelab/static/css/main.54b67edc.chunk.css"
+  },
+  {
+    "revision": "d4ec06d52c065a140a66fa0214cdda72",
+    "url": "/circlelab/index.html"
+  }
+];
