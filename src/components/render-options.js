a92f1fd6268356891e70ecaf9c3d6edf24d7d602
Ignacio Segura <nacho@isegura.es>
2019-04-01 11:06:42 +0200

    Reset and fit

--- src/components/render-options.js
+++ src/components/render-options.js
@@ -7,25 +7,31 @@ class RenderOptions extends Component {
     this.clickHandlerCSSTransform = this.clickHandlerCSSTransform.bind(this);
     this.clickHandlerCSS3DTransform = this.clickHandlerCSS3DTransform.bind(this);
     this.clickHandlerViewBox = this.clickHandlerViewBox.bind(this);
+    this.clickReset = this.clickReset.bind(this);
+    this.SVGScene = null;
+    this.initialViewBox = '';
   }
   clickHandlerCSSTransform(e) {
     e.preventDefault();
+    this.clickReset(e);
     this.SVGScene.style.transform = 'scale(100)';
   }
 
   clickHandlerCSS3DTransform(e) {
     e.preventDefault();
-    this.SVGScene.style.transform = 'perspective(1000px) translateZ(750px)';
+    this.clickReset(e);
+    this.SVGScene.style.transform = 'perspective(1000px) translateZ(900px)';
   }
 
   clickHandlerViewBox(e) {
     e.preventDefault();
+    this.clickReset(e);
     let steps = 50 * 5;
     let currentStep = 0;
     let increment = 1000 / steps;
 
     let interval = setInterval(() => {
-      let axis = currentStep * (increment / 5);
+      let axis = 0;
       let negativeIncrement = 1000 - (currentStep * increment);
 
       let string = axis + ' ' + axis + ' ' + negativeIncrement + ' ' + negativeIncrement;
@@ -36,8 +42,15 @@ class RenderOptions extends Component {
     }, 20);
   }
 
+  clickReset(e) {
+    e.preventDefault();
+    this.SVGScene.style.transform = '';
+    this.SVGScene.setAttribute('viewBox', this.initialViewBox);
+  }
+
   componentDidMount() {
     this.SVGScene = document.getElementById('svg-box');
+    this.initialViewBox = this.SVGScene.getAttribute('viewBox');
   }
 
   render() {
@@ -45,6 +58,7 @@ class RenderOptions extends Component {
       <div onClick={this.clickHandlerCSSTransform}>Zoom by CSS transform</div>
       <div onClick={this.clickHandlerViewBox}>Zoom by viewBox</div>
       <div onClick={this.clickHandlerCSS3DTransform}>Zoom by 3D Transform</div>
+      <div className="reset" onClick={this.clickReset}>Reset</div>
     </div>
   }
 }
